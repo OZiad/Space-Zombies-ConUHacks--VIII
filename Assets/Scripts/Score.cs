33740771f75e2f985860bb5bea768305d54bc1b8
Omar Ziad <omar.ziad1@outlook.com>
2024-01-20 23:27:58 -0500

    Keep track of high score between sessions

--- Assets/Scripts/Score.cs
+++ Assets/Scripts/Score.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+
 public class Score : MonoBehaviour
 {
     public int score = 0;
@@ -9,11 +10,20 @@ public class Score : MonoBehaviour
     public TextMeshProUGUI scoreUI;
     public TextMeshProUGUI highScoreUI;
 
+    private string highScoreKey = "HighScore";
+
+    void Start()
+    {
+        // Load the high score from PlayerPrefs
+        highScore = PlayerPrefs.GetInt(highScoreKey, 0);
+    }
+
     void Update()
     {
         scoreUI.text = "Score: " + score.ToString();
         highScoreUI.text = "High Score: " + highScore.ToString();
     }
+
     public void UpdateScore(int score)
     {
         this.score += score;
@@ -22,6 +32,9 @@ public class Score : MonoBehaviour
         if (this.score > highScore)
         {
             highScore = this.score;
+
+            PlayerPrefs.SetInt(highScoreKey, highScore);
+            PlayerPrefs.Save();
         }
     }
 }
